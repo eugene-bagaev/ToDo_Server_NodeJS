1b309115f406cea69122d500ecec723f988f9a65
Eugen.Bagaev <Eugen.Bagaev@spartasystems.com>
2020-02-24 13:25:57 +0300

    code refactor.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const mongo = require('../routes/db');
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const dbToDo = 'todo';
+
 const collectionWorkplace = 'Workplace';
 const collectionUsers = 'users';
 const collectionNote = 'Note';
-const serverIp = '46.101.211.139';
-const serverPort = '27017';
-const urlToDB = `mongodb://${serverIp}:${serverPort}`;
 
 router.get('/', function(req, res, next) {
 
